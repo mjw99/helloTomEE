c5fbb9985f26a34fb8b8cfd13226c700a004a9cf
Mark Williamson <mjw@mjw.name>
2024-08-14 18:08:23 +0100

    Prune example

--- src/main/java/name/mjw/hellotomee/application/RestEasyApplication.java
+++ src/main/java/name/mjw/hellotomee/application/RestEasyApplication.java
@@ -3,25 +3,15 @@ package name.mjw.hellotomee.application;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.ApplicationPath;
 
-
-
 @ApplicationPath("/foo")
-public class RestEasyApplication extends Application
-{
-    private Set<Class> classes = new HashSet<Class>();
+public class RestEasyApplication extends Application {
+	private Set<Class> classes = new HashSet<Class>();
 
-    public RestEasyApplication() {
-        classes.add(HelloWorld.class);
-    }
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        return super.getClasses();
-    }
+	public RestEasyApplication() {
+		classes.add(HelloWorld.class);
+	}
 
 }
