8cb5f3374fd3eab61ecd494db310428e1abc2d07
Mark Williamson <mjw@mjw.name>
2025-01-23 10:31:29 +0000

    Strip unneeded preamble

--- src/main/java/name/mjw/hellotomee/application/RestEasyApplication.java
+++ src/main/java/name/mjw/hellotomee/application/RestEasyApplication.java
@@ -1,17 +1,9 @@
 package name.mjw.hellotomee.application;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.ApplicationPath;
 
 @ApplicationPath("/foo")
 public class RestEasyApplication extends Application {
-	private Set<Class<?>> classes = new HashSet<>();
-
-	public RestEasyApplication() {
-		classes.add(HelloWorld.class);
-	}
 
 }
